415ff52c24cf04d232fcf8b90428825753683b0e
Mikolaj <snochopl@gmail.com>
2019-05-10 12:13:39 +0200

    MVP border hit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('canvas');
 canvas.width = 600;
 canvas.height = 600;
+let animate = null;
 
 const ctx = canvas.getContext('2d');
 const player = {
@@ -9,7 +10,9 @@ const player = {
   speedVal: 2,
   moveDir: [0, 0],
   draw: () => {
-    requestAnimationFrame(player.draw);
+    animate = requestAnimationFrame(player.draw);
+
+    player.checkPos();
 
     ctx.clearRect(0, 0, 600, 600);
     ctx.beginPath();
@@ -36,6 +39,16 @@ const player = {
       player.moveDir[0] = 0;
       player.moveDir[1] = player.speedVal;
     }
+  },
+  checkPos: () => {
+    if(player.posX <= 0 || player.posX + 20 >= canvas.width || player.posY <= 0 || player.posY + 20 >= canvas.height) {
+      player.gameOver();
+    }
+  },
+  gameOver: () => {
+    cancelAnimationFrame(animate);
+    player.movement = () => {};
+    console.log('GAME OVER!');
   }
 };
 
